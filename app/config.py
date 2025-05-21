e12b591bea04006be18ffafee3bee4dc4c4a7bbc
mbanixox <46408125+mbanixox@users.noreply.github.com>
2025-05-21 14:05:07 +0300

    Add environment config for M-Pesa API

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    MPESA_CONSUMER_KEY: str = os.getenv("MPESA_CONSUMER_KEY")
+    MPESA_CONSUMER_SECRET: str = os.getenv("MPESA_CONSUMER_SECRET")
+    MPESA_BUSINESS_SHORTCODE = os.getenv("MPESA_BUSINESS_SHORTCODE")
+    MPESA_PASSKEY = os.getenv("MPESA_PASSKEY")
+    MPESA_CALLBACK_URL = os.getenv("MPESA_CALLBACK_URL")
+
